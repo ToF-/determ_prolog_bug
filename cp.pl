451b34ef5eba45d0456b65374dbf9e9545fee618
Christophe Thibaut <cthibauttof@gmail.com>
2022-06-29 10:07:00 +0200

    narrowed

--- cp.pl
+++ cp.pl
@@ -1,12 +1,11 @@
 :- module(cp, [f/3]).
 
 f(a,0,42).
-f(a,1,17).
-f(b,0,23).
+f(a,0,43). % this will cause a choicepoint on f(a,0,42).
+f(b,4,42). 
 f(b,5,4807).
-f(c,9,21).
-f(c,11,21).
-:- det(f/3). 
+% :- det(f/3). 
+% :- f(a,0,42), write('*****'), nl, halt.
 
 
 
