38de0b61b909695c3501baa13640bc52c868c1e9
Christophe Thibaut <cthibauttof@gmail.com>
2022-06-29 10:47:43 +0200

    added doc reference about indexing

--- cp.pl
+++ cp.pl
@@ -2,6 +2,7 @@
 
 f(a,0,42).
 f(a,0,43). % this will cause a choicepoint on f(a,0,42).
+% is has to do with indexing https://www.swi-prolog.org/pldoc/man?section=jitindex
 f(b,4,42). 
 f(b,5,4807).
 % :- det(f/3). 
